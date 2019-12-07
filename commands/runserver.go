44b6e0b35840dbf47457a98fe8a3689c1c68cf45
Md Kamrul Islam <kamruli@gmail.com>
2019-12-07 16:55:48 +1100

    Added dockerfile

---
+++ commands/runserver.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to this life-changing API.")
+	})
+
+	fmt.Println("Server listening!")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/kamrulislam/go-docker-mux-redis-notify
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3 // indirect
+require github.com/gorilla/mux v1.7.3
