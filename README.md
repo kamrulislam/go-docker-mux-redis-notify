6cd25ec3fbcf2d7a9afb51412a2fe1ddcd5cd9ad
Md Kamrul Islam <kamruli@gmail.com>
2019-12-08 12:02:24 +1100

    passing email api key through enviroment + added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+# GO Mux Redis Sendgrid Email Docker App
+
+It is a REST Go server which uses Redis to save data and also displays use of a third party email sending using SendGrid. It also provides a docker-compose to build and run all together. 
+
+## Setup
--- commands/send-grid-notifier.go
+++ commands/send-grid-notifier.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sendgrid/sendgrid-go"
+	"os"
 )
 
+//SendGridNotifier : a helper struct to implement Notifier interface
 type SendGridNotifier struct {
 }
 
 //Notify : is a receiver function which also implements Notifier interface
 func (s SendGridNotifier) Notify(customer Customer) error {
-	request := sendgrid.GetRequest("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", "/v3/mail/send", "https://api.sendgrid.com")
+	//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
+	apiKey := os.Getenv("EMAIL_API_KEY")
+	fmt.Println("EMAIL_API_KEY", apiKey)
+	// ideally the API key would be coming from environment
+	request := sendgrid.GetRequest(apiKey, "/v3/mail/send", "https://api.sendgrid.com")
 	request.Method = "POST"
 	request.Body = []byte(` {
 		"personalizations": [
